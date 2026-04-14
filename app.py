b4d6252a84c206a120b3c0ebbf222a588cbe6262
faust65 <151982135+faust65@users.noreply.github.com>
2026-04-14 22:42:29 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, request, jsonify, render_template
+import random
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/atk', methods=['POST'])
+def atk():
+    data=request.get_json()
+    atk=int(data.get('atk', '0'))
+    damage=10
+    
+    g=random.randrange(1, 101)
+    
+    if(g==1):
+        result=f"대성공({g})"
+        damage+=5
+    elif(g==99 or g==100):
+        result=f"대실패({g})"
+        damage-=10
+    elif((atk==1 and g>30) or (atk==2 and g>40) or (atk==3 and g>50) or (atk==4 and g>70) or (atk==5 and g>80)):
+        result=f"실패({g})"
+        damage-=5
+    elif((atk==1 and g<=30) or (atk==2 and g<=40) or (atk==3 and g<=50) or (atk==4 and g<=70) or (atk==5 and g<=80)):
+        result=f"보통 성공({g})"
+        if((atk==1 and g<=15) or (atk==2 and g<=20) or (atk==3 and g<=25) or (atk==4 and g<=35) or (atk==5 and g<=40)):
+            result=f"어려운 성공({g})"
+            damage+=1
+            if((atk==1 and g<=6) or (atk==2 and g<=8) or (atk==3 and g<=10) or (atk==4 and g<=14) or (atk==5 and g<=16)):
+                result=f"극단적 성공({g})"
+                damage+=2
+        
+    return jsonify({'result':result,'damage':damage})
+
+@app.route('/min', methods=['POST'])
+def min():
+    data=request.get_json()
+    min=int(data.get('min', '0'))
+  
+    g=random.randrange(1, 101)
+    
+    if((min==1 and g>30) or (min==2 and g>40) or (min==3 and g>50) or (min==4 and g>70) or (min==5 and g>80)):
+        result=f"실패({g})"
+    elif((min==1 and g<=30) or (min==2 and g<=40) or (min==3 and g<=50) or (min==4 and g<=70) or (min==5 and g<=80)):
+        result=f"성공({g})"
+        
+    return jsonify({'result':result})
+
+@app.route('/run', methods=['POST'])
+def run():
+    data=request.get_json()
+    run=int(data.get('run', '0'))
+    
+    g=random.randrange(1, 101)
+    
+    if((run==1 and g>30) or (run==2 and g>40) or (run==3 and g>50) or (run==4 and g>70) or (run==5 and g>80)):
+        result=f"실패({g})"
+    elif((run==1 and g<=30) or (run==2 and g<=40) or (run==3 and g<=50) or (run==4 and g<=70) or (run==5 and g<=80)):
+        result=f"성공({g})"
+        
+    return jsonify({'result':result})
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
